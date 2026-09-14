87de6fe31cf9d47d6fdaf31ec81a5a454c587e9c
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2026-09-14 21:51:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int search(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+
+    return -1;
+}
