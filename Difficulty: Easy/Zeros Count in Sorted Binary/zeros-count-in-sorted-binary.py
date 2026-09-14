75cca38b6ed80640c317c22f3e9845f29de716d0
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2026-09-14 20:49:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Zeros Count in Sorted Binary/zeros-count-in-sorted-binary.py
+++ Difficulty: Easy/Zeros Count in Sorted Binary/zeros-count-in-sorted-binary.py
@@ -2,7 +2,11 @@ class Solution:
     def countZeroes(self, arr):
         # code here
         c = 0
-        for i in arr:
-            if i == 0:
+        for i in range(len(arr)-1,-1,-1):
+            if arr[i] == 0:
                 c += 1
+            elif arr[i] != 0:
+                break
+            
         return c
+        
