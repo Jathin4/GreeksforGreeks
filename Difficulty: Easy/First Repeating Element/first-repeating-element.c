a14e565cc9da08f3c4835edc8befff303b8cd943
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2026-09-14 22:33:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int firstRepeated(int arr[], int n) {
+    // code here
+    
+    for(int i=0; i<n;i++){
+        
+        for(int j=i+1;j<n;j++){
+            
+            if(arr[i] == arr[j]){
+                return i+1;
+            }
+        }
+    }
+    return -1;
+}
